a804a707fd8857b8acd913843cee909444cc8ec2
Katiana Hanisch <katianahanisch@gmail.com>
2023-11-08 17:01:22 -0400

    ajutes na logica para troca de layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import { useLocation } from "react-router-dom";
-
 import SideBar from "./components/SideBar";
-
 import AppRouter from "./routes";
 
 export default function App() {
-  const location = useLocation();
-
-  const pagina = location.pathname;
+  const { pathname: currentPage } = useLocation();
+  const paginaLogin = currentPage === "/";
 
   return (
     <>
-      {pagina === "/" ? (
-        <AppRouter />
-      ) : (
-        <div className="h-full relative ">
-          <div className="hidden md:flex md:flex-col md:fixed md:w-24 md:inset-y-0 z-[80] border-2 border-r-rose-400">
+      {!paginaLogin && (
+        <div className={`h-full w-full relative `}>
+          <div
+            className={
+              "hidden md:flex md:flex-col md:fixed md:w-24 md:inset-y-0 z-[80] border-2 border-r-rose-400"
+            }
+          >
             <SideBar />
           </div>
           <main className="md:pl-24 h-screen w-full bg-rose-50">
@@ -23,6 +22,8 @@ export default function App() {
           </main>
         </div>
       )}
+
+      {paginaLogin && <AppRouter />}
     </>
   );
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,7 +5,7 @@ export default function Login() {
 
   return (
     <>
-      <section className="bg-gray-200 ">
+      <section className="bg-rose-50">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <a
             href="#"
@@ -18,7 +18,7 @@ export default function Login() {
             />
             Grafica
           </a>
-          <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="w-full bg-white rounded-lg shadow-lg  md:mt-0 sm:max-w-md xl:p-0 ">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                 Faça login em sua conta
