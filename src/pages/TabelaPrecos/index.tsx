bfc5feff1a486e70036482fb0dd34b2f34fc065e
Katiana Hanisch <katianahanisch@gmail.com>
2023-11-11 14:40:46 -0400

    tabela de precos com o crud funcional

--- src/pages/TabelaPrecos/index.tsx
+++ src/pages/TabelaPrecos/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import api from "../../services/api";
 
@@ -6,13 +6,17 @@ import Modal from "../../components/Modal";
 import Input from "../../components/Input";
 import TituloTabela from "../../components/TituloTabela";
 
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import CircularProgress from "@mui/material/CircularProgress";
+
 import { MdModeEditOutline } from "react-icons/md";
 import { BiSolidTrash } from "react-icons/bi";
 
 type TabelaPrecosProps = {
-  id: number;
+  id: number | undefined;
   tipo: string;
-  preco: number;
+  preco: number | undefined;
 };
 
 export default function TabelaPrecos() {
@@ -21,13 +25,52 @@ export default function TabelaPrecos() {
   const [precoProduto, setPrecoProduto] = useState<number | undefined>(
     undefined
   );
+  const [messagemSnackBar, setMessagemSnackBar] = useState<string>("");
+  const [tipoSnackBar, setTipoSnackBar] = useState<string>("");
+
+  const [produtoRemover, setProdutoRemover] = useState<TabelaPrecosProps>({
+    id: undefined,
+    tipo: "",
+    preco: undefined,
+  });
+
+  const [produtoEditando, setProdutoEditando] = useState<TabelaPrecosProps>({
+    id: undefined,
+    tipo: "",
+    preco: undefined,
+  });
+
+  const [carregando, setCarregando] = useState<boolean>(true);
+  const [openSnackBar, setOpenSnackBar] = useState(false);
   const [abrirModalAdicionar, setAbrirModalAdicionar] = useState(false);
   const [abrirModalEditar, setAbrirModalEditar] = useState(false);
   const [abrirModalDeletar, setAbrirModalDeletar] = useState(false);
 
   const tipoUsuario = sessionStorage.getItem("@Auth:TipoUsuario");
 
-  function formatarPreco(valor: number) {
+  const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref
+  ) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenSnackBar(false);
+  };
+
+  function formatarPreco(valor: number | undefined) {
+    if (valor === undefined) {
+      return "";
+    }
+
     return valor.toLocaleString("pt-BR", {
       style: "currency",
       currency: "BRL",
@@ -43,6 +86,15 @@ export default function TabelaPrecos() {
   }
 
   function confirmarModalAdicionar() {
+    if (nomeProduto === "" || precoProduto === undefined) {
+      return;
+    }
+
+    adicionarPreco();
+
+    setNomeProduto("");
+    setPrecoProduto(undefined);
+
     setAbrirModalAdicionar(false);
   }
 
@@ -50,7 +102,16 @@ export default function TabelaPrecos() {
     setAbrirModalEditar(false);
   }
 
-  function confirmarModalEditar() {
+  function confirmarModalEditar(id: number | undefined) {
+    if (produtoEditando.tipo === "" || produtoEditando.preco === 0) {
+      return;
+    }
+
+    if (id !== undefined) {
+      editarProduto(id);
+    }
+
+    setProdutoEditando({ id: undefined, tipo: "", preco: undefined });
     setAbrirModalEditar(false);
   }
 
@@ -58,14 +119,89 @@ export default function TabelaPrecos() {
     setAbrirModalDeletar(false);
   }
 
-  function confirmarModalRemover() {
+  function confirmarModalRemover(id: number | undefined) {
+    if (id !== undefined) {
+      removerProduto(id);
+      getPrecos();
+    }
+
     setAbrirModalDeletar(false);
   }
 
-  async function getPrecos() {
-    const { data } = await api.get("readTabelaDePrecos");
+  async function adicionarPreco() {
+    try {
+      await api.post("createTabelaDePrecos", {
+        tipo: nomeProduto,
+        preco: precoProduto,
+      });
 
-    setDados(data);
+      setTipoSnackBar("sucesso");
+      setMessagemSnackBar("Produto cadastrado com sucesso");
+      setOpenSnackBar(true);
+
+      getPrecos();
+    } catch (error) {
+      setTipoSnackBar("erro");
+      setMessagemSnackBar("Ocorreu um erro ao cadastrar o produto");
+      setOpenSnackBar(true);
+
+      console.error("Erro ao cadastrar produto:", error);
+    }
+  }
+
+  async function editarProduto(id: number) {
+    try {
+      await api.put(`updateTabelaDePrecos/${id}`, {
+        tipo: produtoEditando.tipo,
+        preco: produtoEditando.preco,
+      });
+
+      setTipoSnackBar("sucesso");
+      setMessagemSnackBar("Produto editado com sucesso");
+      setOpenSnackBar(true);
+
+      getPrecos();
+    } catch (error) {
+      setTipoSnackBar("erro");
+      setMessagemSnackBar("Ocorreu um erro ao editar produto");
+      setOpenSnackBar(true);
+      console.error("Erro ao editar produto:", error);
+    }
+  }
+
+  async function removerProduto(id: number) {
+    try {
+      await api.delete(`deleteTabelaDePrecos/${id}`);
+
+      setTipoSnackBar("sucesso");
+      setMessagemSnackBar("Produto deletado com sucesso");
+      setOpenSnackBar(true);
+
+      getPrecos();
+    } catch (error) {
+      setTipoSnackBar("erro");
+      setMessagemSnackBar("Ocorreu um erro ao deletar produto");
+      setOpenSnackBar(true);
+      console.error("Erro ao editar produto:", error);
+    }
+  }
+
+  async function getPrecos() {
+    try {
+      const { data } = await api.get("readTabelaDePrecos");
+
+      setDados(data);
+
+      setCarregando(false);
+    } catch (error) {
+      setTipoSnackBar("erro");
+      setMessagemSnackBar("Ocorreu um erro ao carregar os dados");
+      setOpenSnackBar(true);
+
+      console.log(error);
+
+      setCarregando(false);
+    }
   }
 
   useEffect(() => {
@@ -74,137 +210,183 @@ export default function TabelaPrecos() {
 
   return (
     <div className="w-full h-full flex flex-col items-start">
-      <TituloTabela
-        tipoUsuario={tipoUsuario}
-        titulo="Tabela de Preços"
-        tituloButton="Adicionar produto"
-        abrirModalAdicionar={openModalAdicionar}
-      />
-      <div className=" flex items-center justify-center w-full mt-1 ">
-        <table className={`bg-white w-10/12 divide-y text-left rounded-md`}>
-          <thead className={`bg-rose-500 text-base font-medium `}>
-            <tr>
-              <th className="px-6 py-3">Tipo produto</th>
-              <th className="px-6 py-3">Preço</th>
-              <th className="px-6 py-3"></th>
-              <th className="px-6 py-3"></th>
-            </tr>
-          </thead>
-          <tbody className={`divide-y`}>
-            {dados.map(({ tipo, preco }) => (
-              <>
-                <tr className={` text-sm font-medium`}>
-                  <td className="px-6 py-3">{tipo}</td>
-                  <td className="px-6 py-3">{formatarPreco(preco)}</td>
-                  {tipoUsuario === "empregado" ? null : (
-                    <>
-                      <td className="px-6 py-3">
-                        <button
-                          onClick={() => setAbrirModalEditar(true)}
-                          className="p-1 hover:bg-rose-200 rounded-full transition"
-                        >
-                          <MdModeEditOutline size={21} color="#374151" />
-                        </button>
-                      </td>
-                      <td className="px-6 py-3">
-                        <button
-                          onClick={() => setAbrirModalDeletar(true)}
-                          className="p-1 hover:bg-rose-200 rounded-full transition "
-                        >
-                          <BiSolidTrash size={21} color="#374151" />
-                        </button>
-                      </td>
-                    </>
-                  )}
+      {carregando ? (
+        <div className="flex items-center justify-center w-full h-full">
+          <CircularProgress color="inherit" size={45} />
+        </div>
+      ) : (
+        <>
+          <TituloTabela
+            tipoUsuario={tipoUsuario}
+            titulo="Tabela de Preços"
+            tituloButton="Adicionar produto"
+            abrirModalAdicionar={openModalAdicionar}
+          />
+          <div className="flex items-center justify-center w-full mt-1 ">
+            <table className={`bg-white w-10/12 divide-y text-left rounded-md`}>
+              <thead className={`bg-rose-500 text-base font-medium `}>
+                <tr>
+                  <th className="px-6 py-3">Tipo produto</th>
+                  <th className="px-6 py-3">Preço</th>
+                  <th className="px-6 py-3"></th>
+                  <th className="px-6 py-3"></th>
                 </tr>
-                {abrirModalEditar && (
-                  <Modal
-                    title="Editar Produto"
-                    textButton="Editar"
-                    confirmarModal={confirmarModalEditar}
-                    cancelarModal={fecharModalEditar}
-                    fecharModal={fecharModalEditar}
-                  >
-                    <div className="relative p-6 flex-auto">
-                      <p className="mb-4 text-gray-700 text-lg ">
-                        Detalhes do produto:
-                      </p>
-                      <Input
-                        name="editar"
-                        placeholder="Nome produto"
-                        type="text"
-                        value={nomeProduto}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                          setNomeProduto(e.target.value)
-                        }
-                      />
-                      <Input
-                        name="editar"
-                        placeholder="Preço produto"
-                        type="number"
-                        value={precoProduto || ""}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                          setPrecoProduto(Number(e.target.value))
-                        }
-                      />
-                    </div>
-                  </Modal>
-                )}
-                {abrirModalDeletar && (
-                  <Modal
-                    title="Deseja Confirmar?"
-                    textButton="Remover"
-                    confirmarModal={confirmarModalRemover}
-                    cancelarModal={fecharModalRemover}
-                    fecharModal={fecharModalRemover}
-                  >
-                    <div className="relative p-6 flex-auto">
-                      <p className="text-gray-600 text-lg font-normal leading-relaxed">
-                        Você realmente deseja remover o produto
-                        <span className="font-medium text-gray-700">
-                          {" "}
-                          {tipo}{" "}
-                        </span>
-                        do sistema?
-                      </p>
-                    </div>
-                  </Modal>
-                )}
-              </>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {abrirModalAdicionar && (
-        <Modal
-          title="Adicionar Produto"
-          textButton="Adicionar"
-          confirmarModal={confirmarModalAdicionar}
-          cancelarModal={fecharModalAdicionar}
-          fecharModal={fecharModalAdicionar}
-        >
-          <div className="relative p-6 flex-auto">
-            <p className="mb-4 text-gray-700 text-lg ">Detalhes do produto:</p>
-            <Input
-              name="editar"
-              placeholder="Nome produto"
-              type="text"
-              value={nomeProduto}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setNomeProduto(e.target.value)
-              }
-            />
-            <Input
-              name="editar"
-              placeholder="Preço produto"
-              type="number"
-              value={precoProduto || ""}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setPrecoProduto(Number(e.target.value))
-              }
-            />
+              </thead>
+              <tbody className={`divide-y`}>
+                {dados.map(({ id, tipo, preco }) => (
+                  <React.Fragment key={id}>
+                    <tr className={` text-sm font-medium`}>
+                      <td className="px-6 py-3">{tipo}</td>
+                      <td className="px-6 py-3">{formatarPreco(preco)}</td>
+                      {tipoUsuario === "empregado" ? null : (
+                        <>
+                          <td className="px-6 py-3">
+                            <button
+                              onClick={() => {
+                                setAbrirModalEditar(true);
+                                setProdutoEditando({
+                                  id: id,
+                                  tipo: tipo,
+                                  preco: preco,
+                                });
+                              }}
+                              className="p-1 hover:bg-rose-200 rounded-full transition"
+                            >
+                              <MdModeEditOutline size={21} color="#374151" />
+                            </button>
+                          </td>
+                          <td className="px-6 py-3">
+                            <button
+                              onClick={() => {
+                                setAbrirModalDeletar(true);
+                                setProdutoRemover({
+                                  id: id,
+                                  tipo: tipo,
+                                  preco: preco,
+                                });
+                              }}
+                              className="p-1 hover:bg-rose-200 rounded-full transition "
+                            >
+                              <BiSolidTrash size={21} color="#374151" />
+                            </button>
+                          </td>
+                        </>
+                      )}
+                    </tr>
+                  </React.Fragment>
+                ))}
+              </tbody>
+            </table>
           </div>
-        </Modal>
+          {openSnackBar && (
+            <Snackbar
+              open={openSnackBar}
+              autoHideDuration={3000}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            >
+              <Alert
+                onClose={handleClose}
+                severity={tipoSnackBar === "sucesso" ? "success" : "error"}
+                sx={{ width: "100%" }}
+              >
+                {messagemSnackBar}
+              </Alert>
+            </Snackbar>
+          )}
+          {abrirModalAdicionar && (
+            <Modal
+              title="Adicionar Produto"
+              textButton="Adicionar"
+              confirmarModal={confirmarModalAdicionar}
+              cancelarModal={fecharModalAdicionar}
+              fecharModal={fecharModalAdicionar}
+            >
+              <div className="relative p-6 flex-auto">
+                <p className="mb-4 text-gray-700 text-lg ">
+                  Detalhes do produto:
+                </p>
+                <Input
+                  name="editar"
+                  placeholder="Nome produto"
+                  type="text"
+                  value={nomeProduto}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setNomeProduto(e.target.value)
+                  }
+                />
+                <Input
+                  name="editar"
+                  placeholder="Preço produto"
+                  type="number"
+                  value={precoProduto || ""}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setPrecoProduto(Number(e.target.value))
+                  }
+                />
+              </div>
+            </Modal>
+          )}
+          {abrirModalEditar && (
+            <Modal
+              title="Editar Produto"
+              textButton="Editar"
+              confirmarModal={() => confirmarModalEditar(produtoEditando.id)}
+              cancelarModal={fecharModalEditar}
+              fecharModal={fecharModalEditar}
+            >
+              <div className="relative p-6 flex-auto">
+                <p className="mb-4 text-gray-700 text-lg ">
+                  Detalhes do produto:
+                </p>
+                <Input
+                  name="editar"
+                  placeholder="Nome produto"
+                  type="text"
+                  value={produtoEditando.tipo}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setProdutoEditando((prevProdutoEditando) => ({
+                      ...prevProdutoEditando,
+                      tipo: e.target.value,
+                    }))
+                  }
+                />
+                <Input
+                  name="editar"
+                  placeholder="Preço produto"
+                  type="number"
+                  value={produtoEditando.preco || ""}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setProdutoEditando((prevProdutoEditando) => ({
+                      ...prevProdutoEditando,
+                      preco: Number(e.target.value),
+                    }))
+                  }
+                />
+              </div>
+            </Modal>
+          )}
+          {abrirModalDeletar && (
+            <Modal
+              title="Deseja Confirmar?"
+              textButton="Remover"
+              confirmarModal={() => confirmarModalRemover(produtoRemover.id)}
+              cancelarModal={fecharModalRemover}
+              fecharModal={fecharModalRemover}
+            >
+              <div className="relative p-6 flex-auto">
+                <p className="text-gray-600 text-lg font-normal leading-relaxed">
+                  Você realmente deseja remover o produto
+                  <span className="font-medium text-gray-700">
+                    {" "}
+                    {produtoRemover.tipo}{" "}
+                  </span>
+                  do sistema?
+                </p>
+              </div>
+            </Modal>
+          )}
+        </>
       )}
     </div>
   );
